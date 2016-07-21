ad24cb18569eeb4cfeef02103121d614cb6a6224
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-07-21 13:49:17 -0600

    centered canvas and palette

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
   console.log('sanity check');
 
 
-    for (var i = 0; i < 468; i++) {
+    for (var i = 0; i < 741; i++) {
       var $el = $('<div class="pixel"></div>')
       $('.canvas').append($el);
     };
